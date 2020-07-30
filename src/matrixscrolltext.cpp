9d04bd6436e078c64b6e57987a2b98130d2857a0
hellweek <hellweek@RomoServer.local>
2020-07-30 13:22:48 -0500

    Need this saved

--- src/matrixscrolltext.cpp
+++ src/matrixscrolltext.cpp
@@ -17,6 +17,49 @@ void MatrixScrollText::startShow()
 {
     int bitCounter = 0;
     int row = 0;
+    int startCol = _settings->getStripColumns() - MAXCOLS;
+
+
+      for(int letter = 0; letter < _matrixText.length(); letter++)
+      {
+
+        if((int)_matrixText.toStdString().c_str()[letter] < 32 || (int)_matrixText.toStdString().c_str()[letter] > 122)
+            continue;
+
+        bitCounter = 0;
+        _ledWrapper->clearLeds();
+
+        for(int col = startCol; col < MAXCOLS + startCol; col++)
+        {
+
+
+            for(row = 0; row < MAXROWS; row++)
+            {
+                bitCounter = row * MAXCOLS + col - startCol;
+                if(bitsPerLetter[(int)_matrixText.toStdString().c_str()[letter] - 32][bitCounter] == 1)
+                    _ledWrapper->setPixelColor(row, col, _color1);
+                else
+                    _ledWrapper->setPixelColor(row, col, _ledWrapper->Color(0,0,0));
+                //qDebug() << bitCounter;
+            }
+
+            _ledWrapper->show();
+            Ws2811Wrapper::waitMillSec(_wait);
+
+
+        }
+
+    }
+
+
+}
+
+
+/*
+void MatrixScrollText::startShow()
+{
+    int bitCounter = 0;
+    int row = 0;
 
 
       for(int letter = 0; letter < _matrixText.length(); letter++)
@@ -50,50 +93,4 @@ void MatrixScrollText::startShow()
 
     }
 
-}
-
-/*
-void MatrixScrollText::startShow()
-{
-    int bitCounter = 0;
-
-    while(_endTime > time(nullptr))
-    {
-
-      for(int letter = 0; letter < _matrixText.length(); letter++)
-      {
-
-        if((int)_matrixText.toStdString().c_str()[letter] < 32 || (int)_matrixText.toStdString().c_str()[letter] > 122)
-            continue;
-
-        bitCounter = 0;
-        _ledWrapper->clearLeds();
-        for(int row = 0; row < MAXROWS; row++)
-        {
-            if(_running == false)
-              return;
-
-            for(int col = 5; col < MAXCOLS + 5; col++)
-            {
-
-                if(bitsPerLetter[(int)_matrixText.toStdString().c_str()[letter] - 32][bitCounter] == 1)
-                    _ledWrapper->setPixelColor(row, col, _color1);
-                else
-                    _ledWrapper->setPixelColor(row, col, _ledWrapper->Color(0,0,0));
-                bitCounter++;
-            }
-        }
-
-        _ledWrapper->show();
-        if(_running == false)
-          return;
-         Ws2811Wrapper::waitMillSec(_wait);
-
-      }
-
-    }
-
-}
-
-
-*/
+}*/
