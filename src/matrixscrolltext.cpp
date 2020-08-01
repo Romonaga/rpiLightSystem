96085a2a760c1be7a938c9612572e03a4387c3ec
hellweek <hellweek@RomoServer.local>
2020-08-01 12:40:10 -0500

    Well, at this point I am happy with the results.  Much different then the first stab Well, thats not fair, the first stab did work, but it scrolled your colors off the screen.
    I have not done this type of math in many years, and I am sure someone
    will see a faster and better way.  Fantastic, put a pull request in and I will
    accept it.
    
    The code is as clean as I can get, 100 lines.  My brain hurts, I think i will go take a nap.
    
    Peace out Keep Fluxing!

--- src/matrixscrolltext.cpp
+++ src/matrixscrolltext.cpp
@@ -2,14 +2,12 @@
 #include <QVector>
 #include <math.h>
 #include <QDebug>
-
 #include "font7x6ext.h"
-
-
 
 MatrixScrollText::MatrixScrollText(Ws2811Wrapper* ledWrapper, const LedLightShows &lightShow, const QString &showParms) :
     ILightShow(ledWrapper, lightShow, showParms)
 {
+
     _image = new ws2811_led_t[MAXROWS * _settings->getStripColumns()];
     _rowStart =  (_settings->getStripRows() / 2) - (MAXROWS / 2) - 1;
     _drawCol = _settings->getStripColumns() - 1;
@@ -18,8 +16,9 @@ MatrixScrollText::MatrixScrollText(Ws2811Wrapper* ledWrapper, const LedLightShow
     {
         for(int row = _rowStart; row < (MAXROWS + _rowStart); row++)
         {
-            //qDebug() << "col: " << col << " row: " << (row - _rowStart) << " rowStart: " << _rowStart << " imageLoc: " << ( (row - _rowStart) * _settings->getStripColumns()) + col << " MaxLoc: " << MAXROWS * _settings->getStripColumns();
             _image[( (row - _rowStart) * _settings->getStripColumns()) + col] = _ledWrapper->getPixelColor(row, col);
+            //qDebug() << "col: " << col << " row: " << (row - _rowStart) << " rowStart: " << _rowStart << " imageLoc: " << ( (row - _rowStart) * _settings->getStripColumns()) + col << " MaxLoc: " << MAXROWS * _settings->getStripColumns();
+
         }
     }
 
@@ -32,11 +31,10 @@ MatrixScrollText::~MatrixScrollText()
 
 void MatrixScrollText::shiftColumns()
 {
-    ws2811_led_t color;
-    ws2811_led_t blackColor = _ledWrapper->Color(0,0,0);
+    ws2811_led_t drawColor;
+    ws2811_led_t currentColor;
 
     int past = 0;
-
     for(int current = 0; current  < _settings->getStripColumns() - 1 ; current ++)
     {
 
@@ -44,24 +42,16 @@ void MatrixScrollText::shiftColumns()
         {
             past = current + 1;
 
-            color = _ledWrapper->getPixelColor(row, past);          //Get past
-            _ledWrapper->setPixelColor(row , current, color);       // move to present
-            //_ledWrapper->setPixelColor(row , past, blackColor);     // black hole the past for now.
+            drawColor = _ledWrapper->getPixelColor(row, past);          //Get past color
+            currentColor = _ledWrapper->getPixelColor(row, current);    //get current color
 
+            if( (drawColor != _color1 && currentColor != _color1) || past == 0)
+                drawColor = _image[( (row - _rowStart) * _settings->getStripColumns()) + current];  //replay saved color
 
-         //   if(_image[( (row - _rowStart) * _settings->getStripColumns()) + current] != _color1)
-       //         _ledWrapper->setPixelColor(row , current, _image[( (row - _rowStart) * _settings->getStripColumns()) + current]);    //set to past past colors color
-             _ledWrapper->setPixelColor(row , past, _image[( (row - _rowStart) * _settings->getStripColumns()) + past]);    //set to past past colors color
+            _ledWrapper->setPixelColor(row , current, drawColor);       // move to present
+            _ledWrapper->setPixelColor(row , past, _image[( (row - _rowStart) * _settings->getStripColumns()) + past]);    //set to past past colors color
 
-            //    qDebug() << "shiftColumns-- col: " << past << " row: " << (row - _rowStart) << " rowStart: " << _rowStart << " imageLoc: " << ( (row - _rowStart) * _settings->getStripColumns()) + past << " MaxLoc: " << MAXROWS * _settings->getStripColumns();
-
-
-//            _ledWrapper->setPixelColor(row , past, _image[( (row - _rowStart) * _settings->getStripColumns()) + past]);    //set to past past colors color
-
-            //    qDebug() << "col: " << past << " row: " << (row - _rowStart) << " rowStart: " << _rowStart << " imageLoc: " << ( (row - _rowStart) * _settings->getStripColumns()) + past << " MaxLoc: " << MAXROWS * _settings->getStripColumns();
-
-
-
+            //qDebug() << "past: " <<     (int) pastColor << " current: " << (int) currentColor << " mapColor: " << (int)mapColor << " color1: " << _color1;
         }
     }
 
@@ -72,47 +62,26 @@ void MatrixScrollText::shiftColumns()
 
 void MatrixScrollText::startShow()
 {
-    int bitCounter = 0;
-    int columnStart = MAXCOLS;
 
+    for(int pad = 0; pad < _settings->getStripColumns() / MAXCOLS; pad++)
+        _matrixText.append(" ");
 
-    int drawRow = 0;
-
-
-
-  //  while(_endTime > time(nullptr) && _running == true)
-  //  {
+    while(_endTime > time(nullptr) && _running == true)
+    {
         for(int letter = 0; letter < _matrixText.length(); letter++)
         {
 
             if((int)_matrixText.toStdString().c_str()[letter] < 32 || (int)_matrixText.toStdString().c_str()[letter] > 122)
                 continue;
 
-            bitCounter = 0;
-            for(int col = columnStart; col < (MAXCOLS + columnStart); col++)
+            for(int col = MAXCOLS; col < (MAXCOLS * 2); col++)
             {
-
                 for(int row = 0; row < MAXROWS; row++)
                 {
-                    drawRow = row + _rowStart;
-                    bitCounter = row * MAXCOLS + (col - columnStart);
 
-                    if(letterMatrix[(int)_matrixText.toStdString().c_str()[letter] - 32][bitCounter] == 1)
-                    {
-                        _ledWrapper->setPixelColor(drawRow, _drawCol , _color1);
-                    }
-                    else
-                    {
-                   //    _ledWrapper->setPixelColor(drawRow, _drawCol, _image[( row * _settings->getStripColumns()) + col - columnStart]);
-                      _ledWrapper->setPixelColor(drawRow, _drawCol, _ledWrapper->Color(0,0,0));
-
-                      qDebug() << "startShow-- col: " << col - columnStart << " row: " << row << " rowStart: " << _rowStart << " imageLoc: " << ( row * _settings->getStripColumns()) + col - columnStart  << " MaxLoc: " << MAXROWS * _settings->getStripColumns();
-
-
-                    }
-
+                    if(letterMatrix[(int)_matrixText.toStdString().c_str()[letter] - 32][row * MAXCOLS + (col - MAXCOLS)] == 1)
+                        _ledWrapper->setPixelColor(row + _rowStart, _drawCol , _color1);
                 }
-
 
                 _ledWrapper->show();
                  Ws2811Wrapper::waitMillSec(_wait);
@@ -122,14 +91,11 @@ void MatrixScrollText::startShow()
 
         }
 
+        shiftColumns();
+        if(_running == false)
+          break;
 
-     //   if(_running == false)
-      //    break;
-
-  //  }
-
-
-
+    }
 
 }
 
