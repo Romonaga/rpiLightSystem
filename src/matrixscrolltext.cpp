3f4e868b9fae3125345378287a86aa4b9a88dc8e
hellweek <hellweek@RomoServer.local>
2020-08-01 18:11:08 -0500

    Small changes to fonts,  Added some logging to help others understand logic.
    Still chasing an odd issue that I can not recreate at will.  Hard to track
    No memory issues, but sometimes, the scrolling just stops, and does not include the pad, that is to move it forward in the buffer.
    
    Well, will keep watching.

--- src/matrixscrolltext.cpp
+++ src/matrixscrolltext.cpp
@@ -8,17 +8,18 @@ MatrixScrollText::MatrixScrollText(Ws2811Wrapper* ledWrapper, const LedLightShow
     ILightShow(ledWrapper, lightShow, showParms)
 {
 
+    //We only care about what we might overlay.
     _image = new ws2811_led_t[MAXROWS * _settings->getStripColumns()];
+
     _rowStart =  (_settings->getStripRows() / 2) - (MAXROWS / 2) - 1;
     _drawCol = _settings->getStripColumns() - 1;
 
+    //** WARNING picture time... SNAP!
     for(int col = 0; col < _settings->getStripColumns(); col++)
     {
         for(int row = _rowStart; row < (MAXROWS + _rowStart); row++)
         {
             _image[( (row - _rowStart) * _settings->getStripColumns()) + col] = _ledWrapper->getPixelColor(row, col);
-            //qDebug() << "col: " << col << " row: " << (row - _rowStart) << " rowStart: " << _rowStart << " imageLoc: " << ( (row - _rowStart) * _settings->getStripColumns()) + col << " MaxLoc: " << MAXROWS * _settings->getStripColumns();
-
         }
     }
 
@@ -29,10 +30,13 @@ MatrixScrollText::~MatrixScrollText()
     delete [] _image;
 }
 
+//logic is clear, but we are walking from where we drew the row
+//and we are moving that row forward, and preserving the original color, if it is not a letter's color
 void MatrixScrollText::shiftColumns()
 {
     ws2811_led_t drawColor;
     ws2811_led_t currentColor;
+
 
     int past = 0;
     for(int current = 0; current  < _settings->getStripColumns() - 1 ; current ++)
@@ -42,16 +46,14 @@ void MatrixScrollText::shiftColumns()
         {
             past = current + 1;
 
-            drawColor = _ledWrapper->getPixelColor(row, past);          //Get past color
-            currentColor = _ledWrapper->getPixelColor(row, current);    //get current color
+            drawColor = _ledWrapper->getPixelColor(row, past);                                                              //init drawColor to past color
+            currentColor = _ledWrapper->getPixelColor(row, current);                                                        //get current color
 
-            if( (drawColor != _color1 && currentColor != _color1) || past == 0)
-                drawColor = _image[( (row - _rowStart) * _settings->getStripColumns()) + current];  //replay saved color
+            if( (drawColor != _color1 && currentColor != _color1) )                                                         //replay saved color if needed
+                drawColor = _image[( (row - _rowStart) * _settings->getStripColumns()) + current];
 
-            _ledWrapper->setPixelColor(row , current, drawColor);       // move to present
-            _ledWrapper->setPixelColor(row , past, _image[( (row - _rowStart) * _settings->getStripColumns()) + past]);    //set to past past colors color
-
-            //qDebug() << "past: " <<     (int) pastColor << " current: " << (int) currentColor << " mapColor: " << (int)mapColor << " color1: " << _color1;
+            _ledWrapper->setPixelColor(row , current, drawColor);                                                           // move to present
+            _ledWrapper->setPixelColor(row , past, _image[( (row - _rowStart) * _settings->getStripColumns()) + past]);     //set  past's past color to saved color (yes, I know)..
         }
     }
 
@@ -59,11 +61,12 @@ void MatrixScrollText::shiftColumns()
 }
 
 
-
+//Yes, I know more math.  Trust me, I wrote the damn code, lots of logging, lots of paper!
+//Not much to say here code says it all. but this is where we write to the last secten as centerd to the grid as I can get
 void MatrixScrollText::startShow()
 {
 
-    for(int pad = 0; pad < _settings->getStripColumns() / MAXCOLS; pad++)
+    for(int pad = 0; pad < _settings->getStripColumns() / MAXCOLS; pad++) //pad the string so it will scroll off the screen
         _matrixText.append(" ");
 
     while(_endTime > time(nullptr) && _running == true)
@@ -71,15 +74,16 @@ void MatrixScrollText::startShow()
         for(int letter = 0; letter < _matrixText.length(); letter++)
         {
 
-            if((int)_matrixText.toStdString().c_str()[letter] < 32 || (int)_matrixText.toStdString().c_str()[letter] > 122)
+            if((int)_matrixText.toStdString().c_str()[letter] < 32 || (int)_matrixText.toStdString().c_str()[letter] > 122) //only attempt to print what we know.
                 continue;
+
 
             for(int col = MAXCOLS; col < (MAXCOLS * 2); col++)
             {
                 for(int row = 0; row < MAXROWS; row++)
                 {
 
-                    if(letterMatrix[(int)_matrixText.toStdString().c_str()[letter] - 32][row * MAXCOLS + (col - MAXCOLS)] == 1)
+                    if(letterMatrix[(int)_matrixText.toStdString().c_str()[letter] - 32][row * MAXCOLS + (col - MAXCOLS)] == 1) //should this pixal be on?
                         _ledWrapper->setPixelColor(row + _rowStart, _drawCol , _color1);
                 }
 
